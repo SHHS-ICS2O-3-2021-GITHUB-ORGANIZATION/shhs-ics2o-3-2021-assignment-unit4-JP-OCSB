fea0ed8235310b8d9f33d3b416043b06998d8e67
s285038 <s285038@shha216m56.ocsb.local>
2022-01-26 09:02:43 -0500

    Submitted part of unit 5

---
+++ UNIT 4/U5/Q1Questions.py
@@ -0,0 +1,63 @@
+# NAME OF AUTHOR:  Jacob Pierunek
+# NAME OF THE PROGRAM:  Q1 Questions
+# DATE OF CREATION:  Jan 25, 2022
+# PURPOSE OF PROGRAM:  To create a multiple choice question and save it in a file
+
+#Opening a text file
+filehandle = open("Q1Questions.txt",'w')
+
+# VARIABLE DEFINITION
+
+question = 0
+answera = 0
+answerb = 0
+answerc = 0
+answerd = 0
+rightAnswer = 0
+
+# INPUT
+
+# Prompting user to enter the question
+question = input ("Enter your question: ")
+
+# PROCESSING/INPUT
+
+#Writing question in the text file
+filehandle.write(question + "\n")
+ 
+#Prompting user to enter one possible answers to the question
+answera = input("Enter the first possible answer (a): ")
+
+#Writing the possible answer in the text file
+filehandle.write(answera + "\n")
+
+#Prompting user to enter one possible answers to the question
+answerb = input("Enter the second possible answer (b): ")
+
+#Writing the possible answer in the text file
+filehandle.write(answerb + "\n")
+
+#Prompting user to enter one possible answers to the question
+answerc = input("Enter the third possible answer (c): ")
+
+#Writing the possible answer in the text file
+filehandle.write(answerc + "\n")
+
+#Prompting user to enter one possible answers to the question
+answerd = input("Enter the fourth and final possible answer (d): ")
+
+#Writing the possible answer in the text file
+filehandle.write(answerd + "\n")
+
+#Prompting user to enter the correct answer
+rightAnswer = input("Enter the correct answer in letter form, ex.a: ")
+
+#Writing the correct answer in the text file
+filehandle.write(rightAnswer)
+ 
+#Closing the file
+filehandle.close()
+
+# OUTPUT
+
+print("Yay!!, You have successfully created a multiple choice question")
---
+++ UNIT 4/U5/Q2Quiz.py
@@ -0,0 +1,38 @@
+# NAME OF AUTHOR:  Jacob Pierunek
+# NAME OF THE PROGRAM:  Q2 Quiz
+# DATE OF CREATION:  Jan 26, 2022
+# PURPOSE OF PROGRAM:  To prompt user to answer a previously created multiple choice question
+
+#Open the file
+inFile = open("Q1Questions.txt", 'r')
+
+# VARIABLE DEFINITION
+
+#Reads the lines of the file and assigns them to variables
+line1 = inFile.readline()
+line2 = inFile.readline()
+line3 = inFile.readline()
+line4 = inFile.readline()
+line5 = inFile.readline()
+line6 = inFile.readline()
+userInput = 0
+
+# INPUT/PROCESSING/OUTPUT
+
+#Outputs the file's contents
+print (line1.strip('\n'))
+print ("a) " + (line2.strip('\n')))
+print ("b) " + (line3.strip('\n')))
+print ("c) " + (line4.strip('\n')))
+print ("d) " + (line5.strip('\n')))
+
+#Prompts user for their answer to the question
+userInput = input("Please enter either a, b, c or d and I will tell you if it is correct: ")
+
+#Checks if user answered correctly
+if userInput == line6:
+  print("CORRECT!!!")
+elif userInput != line6:
+  print("You are incorrect, the correct answer was " + line6)
+#Closes file
+inFile.close()
